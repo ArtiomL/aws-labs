c385e40eca262ba197e5baae1adb0c2ba2078af5
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2019-10-17 00:43:29 +0300

    Create an EKS Cluster

--- eks/workshop.sh
+++ eks/workshop.sh
@@ -1,32 +1,23 @@
 #!/usr/bin/env bash
-
-# Create the default ~/.kube directory for storing kubectl configuration
-mkdir -p ~/.kube
+# Create an EKS Cluster
+# https://github.com/ArtiomL/aws-labs
+# Artiom Lichtenstein
+# v1.0.1, 17/10/2019
 
 # Install kubectl
-sudo curl --silent --location -o /usr/local/bin/kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.12.7/2019-03-27/bin/linux/amd64/kubectl
+sudo curl --silent --location -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.13.7/bin/linux/amd64/kubectl
 sudo chmod +x /usr/local/bin/kubectl
-
-# Install AWS IAM Authenticator
-go get -u -v github.com/kubernetes-sigs/aws-iam-authenticator/cmd/aws-iam-authenticator
-sudo mv ~/go/bin/aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
 
 # Install JQ and envsubst
 sudo yum -y install jq gettext
 
 # Verify the binaries are in the path and executable
-for command in kubectl aws-iam-authenticator jq envsubst
+for command in kubectl jq envsubst
   do
     which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
   done
 
 read -s -p "Please verify the binaries are in the path and executable. Press enter to continue "; echo
-
-# Clone the service repos
-cd ~/environment
-git clone https://github.com/brentley/ecsdemo-frontend.git
-git clone https://github.com/brentley/ecsdemo-nodejs.git
-git clone https://github.com/brentley/ecsdemo-crystal.git
 
 # Update IAM settings for your workspace
 read -s -p "Please update IAM settings for your workspace. Press enter to continue "; echo
@@ -46,6 +37,12 @@ aws configure get default.region
 aws sts get-caller-identity
 read -s -p "Please validate the IAM role. Press enter to continue "; echo
 
+# Clone the service repos
+cd ~/environment
+git clone https://github.com/brentley/ecsdemo-frontend.git
+git clone https://github.com/brentley/ecsdemo-nodejs.git
+git clone https://github.com/brentley/ecsdemo-crystal.git
+
 # Generate SSH Key
 ssh-keygen
 
@@ -61,14 +58,16 @@ eksctl version
 read -s -p "Please confirm the eksctl command works. Press enter to continue "; echo
 
 # Create an EKS cluster
-eksctl create cluster --name=eksworkshop-eksctl --nodes=3 --node-ami=auto --region=${AWS_REGION}
+eksctl create cluster --name=eksworkshop-eksctl --nodes=3 --alb-ingress-access --region=${AWS_REGION}
 
 # Confirm your Nodes
 kubectl get nodes
 
 # Export the Worker Role Name for use throughout the workshop
-INSTANCE_PROFILE_NAME=$(aws iam list-instance-profiles | jq -r '.InstanceProfiles[].InstanceProfileName' | grep nodegroup)
-INSTANCE_PROFILE_ARN=$(aws iam get-instance-profile --instance-profile-name $INSTANCE_PROFILE_NAME | jq -r '.InstanceProfile.Arn')
-ROLE_NAME=$(aws iam get-instance-profile --instance-profile-name $INSTANCE_PROFILE_NAME | jq -r '.InstanceProfile.Roles[] | .RoleName')
+STACK_NAME=$(eksctl get nodegroup --cluster eksworkshop-eksctl -o json | jq -r '.[].StackName')
+INSTANCE_PROFILE_ARN=$(aws cloudformation describe-stacks --stack-name $STACK_NAME | jq -r '.Stacks[].Outputs[] | select(.OutputKey=="InstanceProfileARN") | .OutputValue')
+ROLE_NAME=$(aws cloudformation describe-stacks --stack-name $STACK_NAME | jq -r '.Stacks[].Outputs[] | select(.OutputKey=="InstanceRoleARN") | .OutputValue' | cut -f2 -d/)
 echo "export ROLE_NAME=${ROLE_NAME}" >> ~/.bash_profile
 echo "export INSTANCE_PROFILE_ARN=${INSTANCE_PROFILE_ARN}" >> ~/.bash_profile
+
+# You now have a fully working Amazon EKS Cluster
