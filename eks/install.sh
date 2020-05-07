7430ff3bab4fbbc456d46c1b7a923a1db645cd4c
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2020-05-07 17:43:46 +0300

    Install Kubernetes Tools

--- eks/install.sh
+++ eks/install.sh
@@ -2,9 +2,9 @@
 # Install Kubernetes Tools
 # https://github.com/ArtiomL/aws-labs
 # Artiom Lichtenstein
-# v1.2, 07/05/2020
+# v1.3, 07/05/2020
 
-# kubectl
+# Install kubectl
 sudo curl --silent --location -o /usr/local/bin/kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl
 sudo chmod +x /usr/local/bin/kubectl
 
@@ -19,8 +19,14 @@ kubectl completion bash >>  ~/.bash_completion
 . /etc/profile.d/bash_completion.sh
 . ~/.bash_completion
 
-# terraform
+# Install terraform
 wget -O terraform.zip https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip
 unzip terraform.zip
 rm terraform.zip
 sudo mv terraform /usr/local/bin/
+
+# Verify the binaries are in path
+for command in kubectl jq envsubst aws terraform
+  do
+    which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
+  done
