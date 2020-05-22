cc355ca95f56e202b73f561141e553f75b031720
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2020-05-22 15:41:51 +0300

    Install Kubernetes Tools

--- eks/install.sh
+++ eks/install.sh
@@ -2,7 +2,7 @@
 # Install Kubernetes Tools
 # https://github.com/ArtiomL/aws-labs
 # Artiom Lichtenstein
-# v1.3, 07/05/2020
+# v1.5, 22/05/2020
 
 # Install kubectl
 sudo curl --silent --location -o /usr/local/bin/kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl
@@ -28,6 +28,9 @@ wget -O terraform.zip https://releases.hashicorp.com/terraform/0.12.19/terraform
 unzip terraform.zip
 rm terraform.zip
 sudo mv terraform /usr/local/bin/
+
+# Attach the IAM role to your Workspace
+aws ec2 associate-iam-instance-profile --instance-id $(curl -s http://169.254.169.254/latest/meta-data/instance-id) --iam-instance-profile Name=eksworkshop-admin
 
 # Verify the binaries are in path
 for command in kubectl jq envsubst aws aws-iam-authenticator terraform
